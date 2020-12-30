7f1deabdab41bb6ab4071b8b209a47c164615543
Alberto Franzin <franzinalberto01@gmail.com>
2020-12-30 13:14:05 +0100

    laggio 3

--- js/menu_icon.js
+++ js/menu_icon.js
@@ -1,3 +1,11 @@
 function MenuAnimation(x) {
     x.classList.toggle("change");
 }
+
+function ShowHideMenu() {
+    var Menu = document.getElementById('menulaterale');
+    Menu.classList.toggle("menuchange");
+
+    var Scrivania = document.getElementById('scrivania');
+    Scrivania.classList.toggle("scrivaniachange");
+}
